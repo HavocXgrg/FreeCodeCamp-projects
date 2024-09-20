7bafe94b93908ce465dbb32b346c1463a1fa115e
bishwa <108havocx108@gmail.com>
2024-09-20 20:27:41 +0545

    DOM elements retrieval completed

---
+++ Pokemon-Search-App/script.js
@@ -0,0 +1,18 @@
+const userInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const pokemonWeight = document.getElementById("weight");
+const pokemonHeight = document.getElementById("height");
+
+const pokemonImage = document.getElementById("pokemon-image");
+const pokemonTypes = document.getElementById("types");
+
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+
